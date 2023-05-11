3c56d455b05535591499b6f245f88f1cfccf10d9
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2023-05-12 00:21:50 +0300

    chore(eslint): Edited "spellcheck/spell-checker" rule

--- .eslintrc.rule-overrides.js
+++ .eslintrc.rule-overrides.js
@@ -1,1 +1,12 @@
-module.exports = {};
+const { eslint } = require("real-config/api");
+
+const spellChecker = eslint.rules["spellcheck/spell-checker"];
+
+module.exports = {
+  rules: {
+    "spellcheck/spell-checker": [
+      "warn",
+      { ...spellChecker, skipWords: [...spellChecker.skipWords, "wnd"] }
+    ]
+  }
+};
